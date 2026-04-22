bb2d588d2888a617a92d8d04026efafa05e365a7
nimbachi <nimbachi@unicauca.edu.co>
2026-04-22 13:38:25 -0500

    feat: Agregando estructura base para layout y routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { CuentasComponent } from './pages/cuentas/cuentas.component';
+import { MovimientosComponent } from './pages/movimientos/movimientos.component';
+import { ReportesComponent } from './pages/reportes/reportes.component';
+
+export const routes: Routes = [
+
+  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
+
+  { path: 'clientes', component: ClientesComponent },
+
+  { path: 'cuentas', component: CuentasComponent },
+
+  { path: 'movimientos', component: MovimientosComponent },
+
+  { path: 'reportes', component: ReportesComponent }
+
+];
